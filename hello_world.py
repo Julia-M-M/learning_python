78d07f1c0f6371b63c3f8f0dadf53caabc5c76bc
Julia Moreno-Manjón <gulia_94@hotmail.es>
2022-03-16 13:15:31 +0000

    Variables

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,27 @@
-print("Hello world!")
+print("Hello world!") #Write "python hello_world.py" on the terminal to run the script
+
+#Comentario de una sola línea
+"""Comentario de
+múltiples líneas"""
+
+
+        ## DEFINICIÓN DE VARIABLES ##
+    
+    #Variable Texto/String: Cadena de caracteres (letras y símbolos)
+nombre = "Ana" #Texto STRING
+    
+    #Variable numérica
+edad = 20 #INTEGRER number (sin decimales)
+estatura = 1.60 #FLOAT number (decimales)
+    
+    #Variable booleana: sólo puede tener dos valores, Verdadero/Falso --> Se usa para hacer comparaciones/condiciones
+positivo = True #BOOLEAN
+negativo = False #BOOLEAN
+
+#Saber de qué tipo es tu variable
+print(type(nombre))
+print(type(edad))
+print(type(estatura))
+print(type(positivo))
+print(type(negativo))
+
